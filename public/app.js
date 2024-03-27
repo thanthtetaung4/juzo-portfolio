aa78cbe3aaddeb6498da0947b182f23c4d643833
Thant Htet Aung <maung.good@gmail.com>
2024-03-27 12:47:25 +0800

    vite build initial commit

--- public/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-console.log("hello");
-const form = document.querySelector("#contact-form");
-form === null || form === void 0 ? void 0 : form.addEventListener("submit", (e) => {
-    e.preventDefault();
-});
--- src/app.ts
+++
@@ -1,6 +0,0 @@
-console.log("hello");
-const form = document.querySelector("#contact-form");
-
-form?.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
